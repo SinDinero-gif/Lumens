b6596ab16e3ae0c9867cb400750909f13987557d
SinDinero-gif <samuposadalds@gmail.com>
2024-03-07 17:52:43 -0500

    Menu Buttons Animation

--- Assets/Scripts/Managers/Menu/Controllers/LightController.cs
+++ Assets/Scripts/Managers/Menu/Controllers/LightController.cs
@@ -8,17 +8,30 @@ using UnityEngine.Serialization;
 public class LightController : MonoBehaviour
 {
     public GameObject title;
+    public GameObject buttons;
+    private bool _switch = false;
     
     // Start is called before the first frame update
     void Start()
     {
         title.SetActive(false);
+        buttons.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_switch == true)
+        {
+            StartCoroutine(ButtonShow());
+        }
+    }
+
+    private IEnumerator ButtonShow()
+    {
+        _switch = true;
+        yield return new WaitForSeconds(0.8f);
+        buttons.SetActive(true);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -27,6 +40,7 @@ public class LightController : MonoBehaviour
         {
             Debug.Log("Switch");
             title.SetActive(true);
+            _switch = true;
         }
     }
 
